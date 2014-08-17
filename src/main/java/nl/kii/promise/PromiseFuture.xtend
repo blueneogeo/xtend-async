5ea81ae85805a31f1e8f834f178d46bbae932acf
christianvogel <christian.vogel@kimengi.com>
2014-08-18 01:10:40 +0200

    - removed old gatherer code - added some promisefuture docs

--- src/main/java/nl/kii/promise/PromiseFuture.xtend
+++ src/main/java/nl/kii/promise/PromiseFuture.xtend
@@ -2,6 +2,9 @@ package nl.kii.promise
 
 import com.google.common.util.concurrent.AbstractFuture
 
+/**
+ * Converts a promise into a future for legacy code.
+ */
 class PromiseFuture<T> extends AbstractFuture<T> {
 	
 	new(IPromise<T> promise) {
--- src/main/xtend-gen/nl/kii/promise/PromiseFuture.java
+++ src/main/xtend-gen/nl/kii/promise/PromiseFuture.java
@@ -5,6 +5,9 @@ import nl.kii.promise.IPromise;
 import nl.kii.promise.Promise;
 import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 
+/**
+ * Converts a promise into a future for legacy code.
+ */
 @SuppressWarnings("all")
 public class PromiseFuture<T extends Object> extends AbstractFuture<T> {
   public PromiseFuture(final IPromise<T> promise) {
